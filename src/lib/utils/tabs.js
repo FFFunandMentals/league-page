9dfec112041cfd54db8e790d460102ef352b3041
FFFunandMentals <fffunandmentals@gmail.com>
2025-05-31 10:13:23 -0400

    Update tabs.js
    try fixing link to sleeper

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -74,7 +74,8 @@ export const tabs = [
             {
                 icon: 'sports_football',
                 label: 'Go to Sleeper',
-                dest: `https://sleeper.app/leagues/${leagueID}`,
+                //dest: `https://sleeper.app/leagues/${leagueID}`,
+                dest: 'https://sleeper.app/leagues/1180354171184619520',
             },
         ]
     },
